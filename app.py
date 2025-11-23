ac5c034873f455e276e401f34b1b449e9a4dc39a
Riley Zapf <rjzapf@usf.edu>
2025-11-23 17:05:12 -0500

    Add calculation based on user input

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 print("Welcome to my Python program!")
 
 hours = input("How many hours did you work this week? ")
+pay = input("What is your hourly pay rate? ")
+hours = float(hours)
+pay = float(pay)
+
+monthly_pay = hours * 4 * pay
